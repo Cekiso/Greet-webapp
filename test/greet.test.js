895aea59dea296918ac91af8b842a4406d060131
Cekiso <nonkululeko@quirky30.co.za>
2021-09-28 23:45:43 +0200

    all

--- test/greet.test.js
+++ test/greet.test.js
@@ -38,72 +38,60 @@ describe('The basic database web app', function() {
         assert.notEqual(testingGreet.setLanguage('Ntsika', 'English'), 'Hello Ntsika');
         await testingGreet.insertName('Sikelela')
         assert.notEqual(testingGreet.setLanguage('Sikelela', 'English'), 'Hello Sikelela');
-
-        it('should be able to count each user greeted once', function() {
-            let testingGreet = greet();
-            testingGreet.setName('NKULI')
-            testingGreet.setName('nkuli')
-            testingGreet.setName('Nkuli')
-            assert.ok(testingGreet.counter(), 1);
-        });
+    });
 
 
 
-        it('should be able to track all the user names', async function() {
-            let testingGreet = greet(pool);
-            await testingGreet.insertName('Nkuli', 'IsiXhosa');
-            await testingGreet.insertName('Sam', 'IsiXhosa');
-            assert.ok(testingGreet.Names(), ['NKULI', 'SAM']);
+    it('should be able to track all the user names', async function() {
+        let testingGreet = greet(pool);
+        await testingGreet.insertName('Nkuli', 'IsiXhosa');
+        await testingGreet.insertName('Sam', 'IsiXhosa');
+        assert.ok(testingGreet.Names(), ['NKULI', 'SAM']);
+
+
+    });
+    it('should be able to count each user greeted once even if greeted in different languages', async function() {
+        let testingGreet = greet(pool);
+        await testingGreet.insertName('Nkuli', 'Isizulu');
+        await testingGreet.insertName('Nkuli', 'English');
+        await testingGreet.insertName('Nkuli', 'IsiXhosa');
+
+
+    });
+    it('should be able to count each user greeted once', async function() {
+        let testingGreet = greet(pool);
+        await testingGreet.insertName('NKULI')
+        await testingGreet.insertName('nkuli ')
+        await testingGreet.insertName('Nkuli')
+        assert.ok(testingGreet.counter(), 1);
+    });
+
+
+    it('should increment the counter when a new name is greeted', async function() {
+        let testingGreet = greet(pool);
+        await testingGreet.insertName('Luyanda');
+        await testingGreet.insertName('Nkuli');
+        await testingGreet.insertName('Luvo');
+
+        assert.ok(testingGreet.counter(), 3);
+
+
+    });
+    it('should be able to clear the counter', function() {
+        let testingGreet = greet();
+
+        assert.ok(testingGreet.counter(), 0);
+    });
 
 
 
-        });
-        it('should be able to count each user greeted once even if greeted in different languages', async function() {
-            let testingGreet = greet(pool);
-            await testingGreet.insertName('Nkuli', 'Isizulu');
-            await testingGreet.insertName('Nkuli', 'English');
-            await testingGreet.insertName('Nkuli', 'IsiXhosa');
+    it('It should return an error if name is not entered', function() {
+        let testingGreet = greet();
 
-
-        });
-        it('should be able to count each user greeted once', async function() {
-            let testingGreet = greet(pool);
-            await testingGreet.insertName('NKULI')
-            await testingGreet.insertName('nkuli ')
-            await testingGreet.insertName('Nkuli')
-            assert.ok(testingGreet.counter(), 1);
-        });
-
-
-        it('should increment the counter when a new name is greeted', async function() {
-            let testingGreet = greet(pool);
-            await testingGreet.insertName('Luyanda');
-            await testingGreet.insertName('Nkuli');
-            await testingGreet.insertName('Luvo');
-
-            assert.ok(testingGreet.counter(), 3);
-
-
-        });
-        it('should be able to clear the counter', function() {
-            let testingGreet = greet();
-
-            assert.ok(testingGreet.counter(), 0);
-        });
-
-
-
-        it('It should return an error if name is not entered', function() {
-            let testingGreet = greet();
-
-            assert.notEqual(testingGreet.errorHandlingtest('IsiZulu', ''), 'Name is required');
-
-        });
-        after(function() {
-            pool.end();
-        });
         assert.notEqual(testingGreet.errorHandlingtest('IsiZulu', ''), 'Name is required');
 
     });
-
+    after(function() {
+        pool.end();
+    });
 });
